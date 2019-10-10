ebce9992f57f88c0a277f759bbe818c634bacdc8
Alex Berry <alexander_berry@brown.edu>
2019-10-10 13:11:19 -0400

    added code for balance of target variable in eda

--- src/avila_eda.py
+++ src/avila_eda.py
@@ -5,6 +5,7 @@ from os.path import dirname, abspath
 parent_dir = dirname(dirname(abspath(__file__))) #parent directory path
 
 #import raw and preprocessed training data with column names
+#11 features including target variable
 feature_names = ['ic-dist', 'u-margin', 'l-margin', 'exp', 'rows', 'mod-ratio', 'il-space', 'weight', 'peaks', 'mod_over_il-space', 'class']
 dtypes = {'ic-dist': "float64", 
           'u-margin': "float64",
@@ -22,32 +23,35 @@ avila_p = pd.read_csv(parent_dir + "/data/avila_p_train.csv",
                       names=feature_names,
                       dtype=dtypes)
 
-
-#histograms for features
-for feat in avila_p.columns:
-    plt.hist(avila_p[feat],
-             label = feat, 
-             bins = 50)
-    plt.xlabel(feat)
-    plt.ylabel('Count')
-    plt.title('Distribution of ' + feat)
-    plt.tight_layout()
-    plt.savefig(parent_dir + '/figures/' + feat + '_histogram' +'.png',
-                dpi=300)
-    plt.show()
-    
-#correlation matrix
-corrmat = avila_p.corr()
-
-all_cols = corrmat.sort_values('class',ascending=False)['class'].index 
-cols = all_cols[:10] # positively correlated features
-
-pd.plotting.scatter_matrix(avila_p[cols],
-                           c = avila_p['class'],
-                           figsize=(15, 15), marker='o',
-                           hist_kwds={'bins': 20},
-                           s=60,
-                           alpha=.8)
-plt.savefig(parent_dir + '/figures/scattermatrix.png',
-            dpi=300)
-plt.show()
+#balance of target variable classes
+balance = avila_p['class'].value_counts(normalize=True)
+print(balance)
+#
+##histograms for features
+#for feat in avila_p.columns:
+#    plt.hist(avila_p[feat],
+#             label = feat, 
+#             bins = 50)
+#    plt.xlabel(feat)
+#    plt.ylabel('Count')
+#    plt.title('Distribution of ' + feat)
+#    plt.tight_layout()
+#    plt.savefig(parent_dir + '/figures/' + feat + '_histogram' +'.png',
+#                dpi=300)
+#    plt.show()
+#    
+##correlation matrix
+#corrmat = avila_p.corr()
+#
+#all_cols = corrmat.sort_values('class',ascending=False)['class'].index 
+#cols = all_cols[:11] #do not include target variable
+#
+#pd.plotting.scatter_matrix(avila_p[cols],
+#                           #c = avila_p['class'],
+#                           figsize=(15, 15), marker='o',
+#                           hist_kwds={'bins': 20},
+#                           s=60,
+#                           alpha=.8)
+#plt.savefig(parent_dir + '/figures/scattermatrix.png',
+#            dpi=300)
+#plt.show()
